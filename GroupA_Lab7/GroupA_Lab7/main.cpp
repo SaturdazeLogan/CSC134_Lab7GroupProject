c76a1c3afa421a2906a6ea1d3f35123f88df3480
SaturdazeLogan <92831691+SaturdazeLogan@users.noreply.github.com>
2022-04-08 10:14:04 -0400

    Initial file loading, testing GitHub.

---
+++ GroupA_Lab7/GroupA_Lab7/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GroupA_Lab7
+//
+//  Created by Logan Hoffman on 4/8/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    // testing new github repo
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ GroupA_Lab7/GroupA_Lab7/testheader.h
@@ -0,0 +1,12 @@
+//
+//  testheader.h
+//  GroupA_Lab7
+//
+//  Created by Logan Hoffman on 4/8/22.
+//
+
+#ifndef testheader_h
+#define testheader_h
+
+
+#endif /* testheader_h */
