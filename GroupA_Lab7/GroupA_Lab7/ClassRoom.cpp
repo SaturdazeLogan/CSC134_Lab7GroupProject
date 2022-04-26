db5eb39b15cc925bfa705d2bd7a53e240de39a84
justme <newmoneybigbucks@protonmail>
2022-04-26 16:20:28 -0400

    Finished coding student.cpp files

--- GroupA_Lab7/GroupA_Lab7/ClassRoom.cpp
+++ GroupA_Lab7/GroupA_Lab7/ClassRoom.cpp
@@ -2,78 +2,83 @@
 #include<string>
 #include<fstream>
 #include"ClassRoom.h"
+#include "student.h"
 using namespace std;
 
 
 void ClassRoom::read()
 {
-ifstream fin("student.txt");
-int size=0;
-string fname;
-string lname;
-string ssn;
-double grades[4];
-double avg;
-int i=0;
+    ifstream fin ("student.txt"); //
+    int size=0;
+    string fname;
+    string lname;
+    string ssn;
+    double grades[4];
+    double avg;
+    int i=0;
 
-while(fin>>fname)
-{
-fin>>lname;
-fin>>ssn;
-fin>>grades[0];
-fin>>grades[1];
-fin>>grades[2];
-fin>>grades[3];
-s[i].setFName(fname);
-s[i].setLName(lname);
-s[i].setSSN(ssn);
-s[i].setGrades(grades);
-s[i].calAvg();
-i++;
-Student::num++;
+    while(fin>>fname)
+    {
+        fin>>lname;
+        fin>>ssn;
+        fin>>grades[0];
+        fin>>grades[1];
+        fin>>grades[2];
+        fin>>grades[3];
+        s[i].setFirstName(fname);
+        s[i].setLastName(lname);
+        s[i].setSocialSecurity(ssn);
+        s[i].setExamScore(grades);
+        s[i].getAverageScore();
+        i++;
+        Student::num++;
+    }
+    count=Student::num;
 }
-count=Student::num;
-}
+
 void ClassRoom::print()
 {
-for(int i=0;i<count;++i)
-{
-cout<<"STUDENT "<<i+1<<" DATA: \n";
-s[i].print();
+    for(int i=0;i<count;++i)
+    {
+        cout<<"STUDENT "<<i+1<<" DATA: \n";
+        s[i].print();
+    }
 }
-}
-void ClassRoom::avg()
+
+void ClassRoom::calcAvg()
 {
-double sum=0;
-for(int i=0;i<count;++i)
-{
-sum+=s[i].getAvg();
-}
+    double sum=0;
+    for(int i=0;i<count;++i)
+    {
+        sum+=s[i].getAvg();
+    }
 cout<<"AVERAGE IS: "<<sum/count<<endl;
 }
+
 void ClassRoom::sortbyAvg()
 {
-for(int i=0;i<count;++i)
-{
-for(int j=0;j<count-i-1;++j)
-if (s[j].getAvg() > s[j+1].getAvg())
-{
-Student temp = s[j];
-s[j]=s[j+1];
-s[j+1]=temp;
+    for(int i=0;i<count;++i)
+    {
+        for(int j=0;j<count-i-1;++j)
+        if (s[j].getAvg() > s[j+1].getAvg())
+        {
+            Student temp = s[j];
+            s[j]=s[j+1];
+            s[j+1]=temp;
+        }
+    }
 }
-}
-}
+
 void ClassRoom::sortbylastname()
 {
-for(int i=0;i<count;++i)
-{
-for(int j=0;j<count-i-1;++j)
-if (s[j].getLName().compare(s[j+1].getLName())<0)
-{
-Student temp = s[j];
-s[j]=s[j+1];
-s[j+1]=temp;
-}
-}
+    for(int i=0;i<count;++i)
+    {
+        for(int j=0;j<count-i-1;++j)
+        if (s[j].getLName().compare(s[j+1].getLName())<0)
+        {
+            Student temp = s[j];
+            s[j]=s[j+1];
+            s[j+1]=temp;
+        }
+    }
 }
--- GroupA_Lab7/GroupA_Lab7/ClassRoom.h
+++ GroupA_Lab7/GroupA_Lab7/ClassRoom.h
@@ -10,6 +10,35 @@ using namespace std;
 
 class ClassRoom
 {
+public:
+    string classRoomName;
+    int numOfStudentObjects;
+    Student StudentArray[24];
+
+
+    ClassRoom(string classRoomName);
+
+    Student* getStudentArray();
+    Student setStudentArray(Student StudentArray[24], int numberofStudenObjects);
+
+    int setNumStudentObjects();
+    int getNumStudentObjects();
+
+    string setClassRoomName(string classRoomName);
+
+    string getClassRoomName();
+
+    void read();
+
+    void sortbyAvg();
+
+    void sortbylastname();
+
+    void calcAvg();
+
+    void displayAllStudenInfo();
+
+    int returnNumStudentsinclass();
 
 };
 #endif //CSC134_LAB7GROUPPROJECT_CLASSROOM_H
