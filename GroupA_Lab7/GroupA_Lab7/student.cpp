066aa35f93d580a66759463d8f6f2421aa8b922f
justme <newmoneybigbucks@protonmail>
2022-04-26 18:05:30 -0400

    finished and almost working student.cpp and.h

--- GroupA_Lab7/GroupA_Lab7/student.cpp
+++ GroupA_Lab7/GroupA_Lab7/student.cpp
@@ -1,128 +1,147 @@
 //
 // Created by justme on 4/15/22.
 //
+#include <iostream>
+#include <iomanip>
 #include "student.h"
 using namespace std;
 
- static int numberOfStudents = 0;
+static int numberOfStudents = 0;
 
- Student::Student() {
-     firstName = "";
-     lastName = "";
-     social_security = "";
-     examScore[] = {0.0, 0.0, 0.0, 0.0};
-     studentID = 0;
+Student::Student() {
+    firstName = "";
+    lastName = "";
+    social_security = "";
+    double examScore[4] = {0.0, 0.0, 0.0, 0.0};
+    studentID = 0;
 
- }
+}
 
- Student::Student(string fN,string lN, string socialSN, double examScoreTempArray[], int stuID) {
-     firstName = fn;
-     lastName = lN;
-     social_security = socialSN;
-     examScore[4] = examScoreTempArray;
-     studentID = stuID;
-     *numberOfStudents += 1
- }
+Student::Student(string fN, string lN, string socialSN, double examScoreTempArray[4], int stuID) {
+    firstName = fN;
+    lastName = lN;
+    social_security = socialSN;
+    examScore[4] = examScoreTempArray[4];
+    studentID = stuID;
+    numberOfStudents += 1;
+}
 
- string Student::getFirstName() {
+string Student::getFirstName() {
 
-     return firstName;
- }
+    return firstName;
+}
 
- string Student::getLastName() {
-     return lastName;
- }
- string Student::getSocialSecurity() {
-     return  social_security;
- }
+string Student::getLastName() {
+    return lastName;
+}
+string Student::getSocialSecurity() {
+    return  social_security;
+}
 
- double Student::getExamScore(int examNum) {
-     int examIndex = examNum - 1;
-     return *examScore[examIndex];
- }
+double Student::getExamScore(int examNum) {
+    int examIndex = examNum - 1;
+    return examScore[examIndex];
+}
 
- double* Student::getExamScoreArray(){
-     return examScore;
- }
- // Setter functions.
-
-
- void Student::setFirstName(string *firstNameInbound) {
-
-     firstName = firstNameInbound;
-
- }
+double * Student::getExamScoreArray(){
+    return examScore;
+}
+// Setter functions.
 
 
- // This funciton sets the last name for our student object
+void Student::setFirstName(string firstNameInbound) {
 
- void Student::setLastName(string *lastNameInbound) {
-     lastName = lastNameInbound;
- }
+    firstName = firstNameInbound;
 
- // this function sets the social security number variable for the student object
-
- void Student::setSocialSecurity(string *socialSecurityInbound) {
-     social_security = socialSecurityInbound;
- }
-
- void Student::setExamScore(double scores[]) {
-
-     for (int i = 0; i < 4; i++)
-     {
-         examScore[i] = scores[i];
-     }
- }
-
- double Student::getAverageScore() {
-     double examTotal = 0.00;
-     double examAverage = 0.00;
-
-     int length = sizeof(examScore)/sizeof(examScore[0]);  // sizeof yeilds the size in bytes of the data type. This gives 32 bytes/ 8 or uint 4
-
-     for(int i=0; i <= length; i++){
-
-         examTotal = examTotal + examScore[i]; // could just use += here
-
-     }
-
-     examAverage = examTotal/4;
-
-     return examAverage;
-
- }
-
- void Student::displayBasicStats() {
-     double examTotal = 0.00;
-     double examAverage = 0.00;
+}
 
 
+// This funciton sets the last name for our student object
 
-     for(int i=0; i < 4; i++){
+void Student::setLastName(string lastNameInbound) {
+    lastName = lastNameInbound;
+}
 
+// this function sets the social security number variable for the student object
 
-         examTotal += examScore[i];
+void Student::setSocialSecurity(string socialSecurityInbound) {
+    social_security = socialSecurityInbound;
+}
 
-     }
-     std::cout
-        << setw(20) << "Last Name" << setw(20) << "First Name" << setw(20) << "SSN" << setw(10) << "score 1"
-        << setw(10) << "Score 2" << setw(10) << "score 3" << setw(10) << "score 4" << setw(10) << "average" << '\n';
+void Student::setExamScore(double scores[]) {
 
-     examAverage = examTotal/4;
+    for (int i = 0; i < 4; i++)
+    {
+        examScore[i] = scores[i];
+    }
+}
 
-     std:cout
-        << setw(20) << lastName << setw(20) << firstName << setw(20) << social_security <<;
-     for (int i = 0; i < 4; i++)
-     {
-         cout << setw(10) << examScore[i];
-     }
-     cout << setw(10) << examAverage;
-     /*setw(10) << "score 1"
-        << setw(10) << "Score 2" << setw(10) << "score 3" << setw(10) << "score 4" << setw(10) << "average" << '\n';*/
+double Student::getAverageScore() {
+    double examTotal = 0.00;
+    double examAverage = 0.00;
+
+    int length = sizeof(examScore)/sizeof(examScore[0]);  // sizeof yeilds the size in bytes of the data type. This gives 32 bytes/ 8 or uint 4
+
+    for(int i=0; i <= length; i++){
+
+        examTotal = examTotal + examScore[i]; // could just use += here
+
+    }
+
+    examAverage = examTotal/4;
+
+    return examAverage;
+
+}
+
+void Student::displayBasicStats() {
+    double examTotal = 0.00;
+    double examAverage = 0.00;
 
 
 
-     /*
+    for(int i=0; i < 4; i++){
+
+
+        examTotal += examScore[i];
+
+    }
+    std::cout
+            << setw(20) << "Last Name" << setw(20) << "First Name" << setw(20) << "SSN" << setw(10) << "score 1"
+            << setw(10) << "Score 2" << setw(10) << "score 3" << setw(10) << "score 4" << setw(10) << "average" << '\n';
+
+    examAverage = examTotal/4;
+
+    std:cout
+        << setw(20) << lastName << setw(20) << firstName << setw(20) << social_security << '\n';
+    for (int i = 0; i < 4; i++)
+    {
+        cout << setw(10) << examScore[i];
+    }
+    cout << setw(10) << examAverage;
+    /*setw(10) << "score 1"
+       << setw(10) << "Score 2" << setw(10) << "score 3" << setw(10) << "score 4" << setw(10) << "average" << '\n';*/
+
+
+
+    /*
+    cout << "Last Name" <<
+    cout << "\nStudent first name: " << firstName;
+    cout << "\nStudent last name: " << lastName;
+    cout << "\nStudent exam average: " << examAverage;
+    cout << "\nExam score list: " << examScore[0] << ", " << examScore[1] << ", " << examScore[2] << ", " << examScore[3] << endl;
+   */
+}
+
+
+int Student::getStudentID() {
+
+    return studentID;
+}
+
+//Student::Student(string basicString, string basicString1, string basicString2, double *pDouble) {
+
+//}
      cout << "Last Name" <<
      cout << "\nStudent first name: " << firstName;
      cout << "\nStudent last name: " << lastName;
--- GroupA_Lab7/GroupA_Lab7/student.h
+++ GroupA_Lab7/GroupA_Lab7/student.h
@@ -8,6 +8,16 @@
 #ifndef student_h
 #define student_h
 #include<iostream>
+//
+//  student.h
+//  GroupA_Lab7
+//
+//  Created by Logan Hoffman on 4/12/22.
+//
+
+#ifndef student_h
+#define student_h
+#include<iostream>
 #include<string>
 using namespace std;
 
@@ -18,146 +28,79 @@ public:
     // the student.cpp file. It has made this file significatnyl shorter. You will notice that I have left the bits that were initially in the file
     // in the file, but I just commented them out. When this project is to be turned in they should be deleted, but I left them for the time being.
     //Data memeber initialization
-    
+
+    //Student(string basicString, string basicString1, string basicString2, double pDouble[4]);
+
     string firstName = "";
     string lastName = "";
     string social_security = "";
     double examScore[4];
     int studentID;
-    
+
 
     // Constructor: no parameters
     Student();//{
     //}
-    
-    
+
+
     // Constructor: with parameters for setup
-    Student(string fN, string lN, string socialSN, double examScoreTempArray[], int stuID);
-    /*{
-        
-        firstName = fN;
-        lastName = lN;
-        social_security = socialSN;
-        studentID = stuID;
-        
-        
-        
-        for(int i=0; i < 4; i++){
-            
-            examScore[i] = examScoreTempArray[i];
-            
-        }
-        
-    }*/
-    
-    
+    Student(string fN, string lN, string socialSN, double examScoreTempArray[4], int stuID);
+
+
+
+
+
     // Destructor: NEEDS AN UPDATE/BUG FIX
     //~Student();
-    
+
     // Getter functions for all member variables
-    
+
     string getFirstName();//{
-        
-       // return firstName;
-        
-    //}
-    
+
+
+
     string getLastName();//{
-        
-        //return lastName;
-        
-    //}
-    
+
+
+
     string getSocialSecurity();//{
-        
-        //return social_security;
-        
-   // }
-    
+
+
+
     double getExamScore(int examNum);//{
-        
-       // return examScore[examNum];
-        
-   // }
-   double* getExamScoreArray();
-    // Setter functions for all member variables
-    
-    void setFirstName(string *firstNameInbound);//{
-        
-    //    firstName = firstNameInbound;
-        
-    //}
-    
-    void setLastName(string *lastNameInbound);//{
-        
-       // lastName = lastNameInbound;
-        
-   // }
-    
-    void setSocialSecurity(string *socialSecurityInbound);//{
-        
-        //social_security = socialSecurityInbound;    // Do most of these setters need to be pointers.
-        
+
+
+    double * getExamScoreArray();
+
+    void setFirstName(string firstNameInbound);//{
+
+
+
+    void setLastName(string lastNameInbound);//{
+
+
+
+    void setSocialSecurity(string socialSecurityInbound);//{
+
+
+
    // }
     void setExamScore(double scores[]);
     // In the insturctions the professor singled out that we need to have the function that sets
     // the values of the exam score array use an array as input.
 
 
-   /* void setExamScore(int examNum, double examScoreInbound){
-        
-        examScore[examNum] = examScoreInbound;
-        
-    }
-    */
-    //getAverageScore calculates and then returns the exam average of the student.
-    
+
     double getAverageScore();
     /*{
-        
-        double examTotal = 0.00;
-        double examAverage = 0.00;
-        
-        int length = sizeof(examScore)/sizeof(examScore[0]);  // sizeof yeilds the size in bytes of the data type. This gives 32 bytes/ 8 or uint 4
-        
-        for(int i=0; i <= length; i++){
-            
-            examTotal = examTotal + examScore[i]; // could just use += here
-            
-        }
-        
-        examAverage = examTotal/4;
-        
-        return examAverage;
-        
-    }*/
-    
-    //displayBasicStats displays the first and last name, exam scores and exam average to the console.
-    
-    void displayBasicStats();
-    /*{
-        
-        double examTotal = 0.00;
-        double examAverage = 0.00;
-        
 
-        
-        for(int i=0; i < 4; i++){
-        
-            
-            examTotal += examScore[i];
-            
-        }
-        
-        examAverage = examTotal/4;
-        
-        cout << "\nStudent first name: " << firstName;
-        cout << "\nStudent last name: " << lastName;
-        cout << "\nStudent exam average: " << examAverage;
-        cout << "\nExam score list: " << examScore[0] << ", " << examScore[1] << ", " << examScore[2] << ", " << examScore[3] << endl;
-        
+
     }*/
-    
+
+    //displayBasicStats displays the first and last name, exam scores and exam average to the console.
+
+    void displayBasicStats();
+
     //Returns student object ID
     
     int getStudentID();//{
